4584e90a1fe45e234b9e1dbe99966b752789902e
xzh <1244838363@qq.com>
2021-03-07 19:38:18 +0800

    add utils and 226

---
+++ utils/src/utils/ListNode.java
@@ -0,0 +1,43 @@
+package utils;
+
+/**
+ * @author xing
+ * @create 2021-03-07 19:12
+ */
+public class ListNode {
+    char val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(char val) {
+        this.val = val;
+    }
+
+    ListNode(char val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                '}';
+    }
+
+    public ListNode constructlist(String str) {
+        int last = 0;
+        ListNode[] list = new ListNode[10];
+        for (int i = 0; i < str.length(); i++) {
+            list[i] = new ListNode(str.charAt(i));
+        }
+        for (int i = 0; i < str.length() - 1; i++) {
+            last = i;
+            list[i].next = list[i + 1];
+        }
+        list[++last] = null;
+        return list[0];
+    }
+}

ef7ea20cfc2dd17c405113d29be42d4308ca9954
xzh <1244838363@qq.com>
2021-03-08 20:48:21 +0800

    modify TreeNode public

--- utils/src/utils/TreeNode.java
+++ utils/src/utils/TreeNode.java
@@ -11,17 +11,17 @@ import java.util.Queue;
  */
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode() {
     }
 
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;

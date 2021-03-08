9a6d182aa8cfa4305a5b4344b5226caeb5e1fdf7
xzh <1244838363@qq.com>
2021-03-08 23:03:31 +0800

    new leetcode part

---
+++ leetcodepart2/src/com/java/binarytree/ConstructBTree106.java
@@ -0,0 +1,56 @@
+package com.java.binarytree;
+
+import com.java.utils.TreeNode;
+/**
+ * @author xing
+ * @create 2021-03-08 23:02
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution {
+    int rightsize = 0;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    public TreeNode build(int[] inorder, int iStart, int iEnd, int[] postorder, int pStart, int pEnd){
+        //特殊情况
+        if(iStart > iEnd || pStart > pEnd){
+            return null;
+        }
+
+
+        //找到根节点所在的索引
+        int index = iStart;
+        for(int i = iEnd; i >= iStart; i--){
+            if(inorder[i] == postorder[pEnd]){
+                index = i;
+                break;
+            }
+        }
+        //获取当前所需构建的右子树长度
+        int rightsize = iEnd - index;
+
+        //构建根节点
+        TreeNode root = new TreeNode(inorder[index]);
+        root.right = build(inorder, index + 1, iEnd, postorder, pEnd - rightsize, pEnd - 1);
+        //构建左子树时还需减去右子树的长度
+        root.left = build(inorder, iStart, index - 1, postorder, pStart, pEnd - rightsize - 1);
+        return root;
+    }
+}
---
+++ leetcodepart2/src/com/java/utils/TreeNode.java
@@ -0,0 +1,96 @@
+package com.java.utils;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * 二叉树题目的一个难点就是，如何把题目的要求细化成每个节点需要做的事情。
+ * @author xing
+ * @create 2021-03-07 19:16
+ */
+public class TreeNode {
+    int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (node == null) {
+                output += "null, ";
+                continue;
+            }
+
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+}

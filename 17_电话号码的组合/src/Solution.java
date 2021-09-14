4a9335651dfdb5acbccb852a1bff7385599b42c9
xing <1244838363@qq.com>
2021-09-14 23:56:31 +0800

    commit 9.14 leetcode

--- 17_电话号码的组合/src/Solution.java
+++ 17_电话号码的组合/src/Solution.java
@@ -9,30 +9,30 @@ public class Solution {
     //方法一
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
-        if(digits == null || digits.length() == 0){
+        if (digits == null || digits.length() == 0) {
             return result;
         }
-        String[] strs = new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        String[] strs = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         //1.先算出一共有几种
         int len = 1;
-        for(int i = 0; i < digits.length(); i++){
-            int c = digits.charAt(i)-'0';
+        for (int i = 0; i < digits.length(); i++) {
+            int c = digits.charAt(i) - '0';
             len *= strs[c].length();
         }
 
 //再用求余方法拿到每一种
         /*
-        * 以"23"为例，可能种类有3*3=9种，对应00，01，02，10，11，12，20，21，22
-        * 0~9 / 3 的范围为 0~2
-        * 0~9 % 3 的范围为 0~2，
-        * 因此可以遍历得到所有的可能
-        * 类似于十进制到二进制的转换：
-        * 10 / 2 余 0，商5
-        * 5 / 2 余 1，商2
-        * 2 / 2 余 0，商1
-        * 1 / 2 余 1，商0
-        * 得到结果1010
-        * 每一个可能都对应此种类似进制转换下的一个值，求模后继续用商进行求模*/
+         * 以"23"为例，可能种类有3*3=9种，对应00，01，02，10，11，12，20，21，22
+         * 0~9 / 3 的范围为 0~2
+         * 0~9 % 3 的范围为 0~2，
+         * 因此可以遍历得到所有的可能
+         * 类似于十进制到二进制的转换：
+         * 10 / 2 余 0，商5
+         * 5 / 2 余 1，商2
+         * 2 / 2 余 0，商1
+         * 1 / 2 余 1，商0
+         * 得到结果1010
+         * 每一个可能都对应此种类似进制转换下的一个值，求模后继续用商进行求模*/
 //        for(int i = 0 ; i < len; i++){
 //            int last = i;
 //            StringBuilder sb = new StringBuilder();
@@ -44,14 +44,14 @@ public class Solution {
 //            }
 //            result.add(sb.toString());
 //        }
-        for(int i = 0 ; i < len; i++){
+        for (int i = 0; i < len; i++) {
             int last = i;
             StringBuilder sb = new StringBuilder();
-            for(int j = digits.length()-1; j>=0;j--){
-                int c = digits.charAt(j)-'0';
-                int pos = last%strs[c].length();// 取其余数
+            for (int j = digits.length() - 1; j >= 0; j--) {
+                int c = digits.charAt(j) - '0';
+                int pos = last % strs[c].length();// 取其余数
                 sb.append(strs[c].charAt(pos));
-                last = last/strs[c].length();//？？？？类似于十进制到二进制的转换，00，01
+                last = last / strs[c].length();//？？？？类似于十进制到二进制的转换，00，01
             }
             result.add(sb.reverse().toString());
         }
@@ -92,6 +92,7 @@ public class Solution {
             }
         }
     }
+
     public static void main(String[] args) {
         String digits = "23";
         Solution solution = new Solution();
@@ -100,8 +101,9 @@ public class Solution {
         System.out.println(ans.toString());
         System.out.println(ans1.toString());
     }
+
     @Test
-    public void test1(){
+    public void test1() {
         System.out.println('A' - '0');
     }
 }

bf440b4f5b168604693730c5a11c6d39a0184a1a
xing <1244838363@qq.com>
2021-09-26 17:56:18 +0800

    commit 9.26 leetcode

--- leetcodepart2/src/com/java/hot100/WordSearch79.java
+++ leetcodepart2/src/com/java/hot100/WordSearch79.java
@@ -6,7 +6,14 @@ package com.java.hot100;
  *
  * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
  *
- * 链接：https://leetcode-cn.com/problems/word-search
+ * 思路：
+ * 设函数check(i, j, k)表示从网格的（i， j）位置出发，判断是否能搜索到单词word[k...],其中word[k...]表示字符串word从k个字符开始的后缀字符串，
+ * 如果能搜索到，则返回 true，反之返回 false
+ * check(i, j, k)执行步骤：
+ * 如果board[i][j] != s[k], return false；
+ * 如果当前已经访问到字符串的末尾，且对应字符依然匹配，此时直接返回true。
+ * 否则，遍历当前位置的相邻位置，若从某个相邻位置出发，能够搜索到子串word[k+1...],则返回true，否则返回false
+ * 对每一个位置都调用check(i,j,0)进行检查
  * @author xing
  * @create 2021-05-11 21:23
  */
